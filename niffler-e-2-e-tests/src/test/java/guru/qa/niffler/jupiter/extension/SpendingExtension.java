3098428b8c613df7ac8daca75fe9c0de256db636
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2025-07-21 18:50:43 +0300

    lesson_3.3_hw - complete

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/SpendingExtension.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/jupiter/extension/SpendingExtension.java
@@ -1,14 +1,10 @@
 package guru.qa.niffler.jupiter.extension;
 
 import guru.qa.niffler.api.SpendApiClient;
-import guru.qa.niffler.jupiter.annotation.Spending;
+import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.model.CategoryJson;
 import guru.qa.niffler.model.SpendJson;
-import org.junit.jupiter.api.extension.BeforeEachCallback;
-import org.junit.jupiter.api.extension.ExtensionContext;
-import org.junit.jupiter.api.extension.ParameterContext;
-import org.junit.jupiter.api.extension.ParameterResolutionException;
-import org.junit.jupiter.api.extension.ParameterResolver;
+import org.junit.jupiter.api.extension.*;
 import org.junit.platform.commons.support.AnnotationSupport;
 
 import java.util.Date;
@@ -22,32 +18,28 @@ public class SpendingExtension implements BeforeEachCallback, ParameterResolver 
   private final SpendApiClient spendApiClient = new SpendApiClient();
 
   @Override
-  public void beforeEach(ExtensionContext context) throws Exception {
-    AnnotationSupport.findAnnotation(
-        context.getRequiredTestMethod(),
-        Spending.class
-    ).ifPresent(
-        anno -> {
-          SpendJson spendJson = new SpendJson(
-              null,
-              new Date(),
-              new CategoryJson(
-                  null,
-                  anno.category(),
-                  anno.username(),
-                  false
-              ),
-              anno.currency(),
-              anno.amount(),
-              anno.description(),
-              anno.username()
-          );
-          context.getStore(NAMESPACE).put(
-              context.getUniqueId(),
-              spendApiClient.addSpend(spendJson)
-          );
-        }
-    );
+  public void beforeEach(ExtensionContext context) {
+    AnnotationSupport.findAnnotation(context.getRequiredTestMethod(), User.class)
+        .ifPresent(anno -> {
+          if (anno.spendings().length > 0) {
+            SpendJson spendJson = new SpendJson(
+                null,
+                new Date(),
+                new CategoryJson(
+                    null,
+                    anno.spendings()[0].category(),
+                    anno.username(),
+                    false
+                ),
+                anno.spendings()[0].currency(),
+                anno.spendings()[0].amount(),
+                anno.spendings()[0].description(),
+                anno.username()
+            );
+            SpendJson created = spendApiClient.addSpend(spendJson);
+            context.getStore(NAMESPACE).put(context.getUniqueId(), created);
+          }
+        });
   }
 
   @Override
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/SpendingTest.java
@@ -3,7 +3,9 @@ package guru.qa.niffler.test.web;
 import com.codeborne.selenide.Selenide;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.Spending;
+import guru.qa.niffler.jupiter.annotation.User;
 import guru.qa.niffler.jupiter.annotation.meta.WebTest;
+import guru.qa.niffler.model.CurrencyValues;
 import guru.qa.niffler.model.SpendJson;
 import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.Test;
@@ -13,11 +15,14 @@ public class SpendingTest {
 
   private static final Config CFG = Config.getInstance();
 
-  @Spending(
+  @User(
       username = "duck",
-      amount = 89990.00,
-      description = "Advanced 9 поток!",
-      category = "Обучение"
+      spendings = @Spending(
+          category = "Обучение",
+          description = "Обучение Niffler 2.0",
+          amount = 89000.00,
+          currency = CurrencyValues.RUB
+      )
   )
   @Test
   void mainPageShouldBeDisplayedAfterSuccessLogin(SpendJson spendJson) {
