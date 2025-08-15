8ad203d7197a3eec237313670ecd2a9e0bb4d66f
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2025-08-15 20:30:22 +0300

    lesson_6.1_hw - complete

---
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/data/repository/UserdataUserRepository.java
@@ -0,0 +1,26 @@
+package guru.qa.niffler.data.repository;
+
+import guru.qa.niffler.data.entity.userdata.UserdataUserEntity;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserdataUserRepository {
+
+  UserdataUserEntity create(UserdataUserEntity user);
+
+  Optional<UserdataUserEntity> findById(UUID id);
+
+  Optional<UserdataUserEntity> findByUsername(String username);
+
+  void delete(UserdataUserEntity user);
+
+  List<UserdataUserEntity> findAll();
+
+  void addIncomeInvitation(UserdataUserEntity requester, UserdataUserEntity addressee);
+
+  void addOutcomeInvitation(UserdataUserEntity requester, UserdataUserEntity addressee);
+
+  void addFriend(UserdataUserEntity requester, UserdataUserEntity addressee);
+}
