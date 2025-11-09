19040ceee9c6e0bb1baad46e4573b858743bcd9a
alekulikov <aleksandr.a.kulikov@mvideo.ru>
2025-11-09 16:58:28 +0300

    lesson_10.2

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/LoginPage.java
@@ -1,6 +1,8 @@
 package guru.qa.niffler.page;
 
+import com.codeborne.selenide.SelenideDriver;
 import com.codeborne.selenide.SelenideElement;
+import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 
@@ -11,10 +13,25 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class LoginPage extends BasePage<LoginPage> {
 
-  private final SelenideElement usernameInput = $("input[name='username']");
-  private final SelenideElement passwordInput = $("input[name='password']");
-  private final SelenideElement submitButton = $("button[type='submit']");
-  private final SelenideElement registerLink = $(By.linkText("Create new account"));
+  private final SelenideElement usernameInput;
+  private final SelenideElement passwordInput;
+  private final SelenideElement submitButton;
+  private final SelenideElement registerLink;
+
+  public LoginPage(SelenideDriver driver) {
+    this.usernameInput = driver.$("input[name='username']");
+    this.passwordInput = driver.$("input[name='password']");
+    this.submitButton = driver.$("button[type='submit']");
+    this.registerLink = driver.$(By.linkText("Create new account"));
+    WebDriverRunner.setWebDriver(driver.getWebDriver());
+  }
+
+  public LoginPage() {
+    this.usernameInput = $("input[name='username']");
+    this.passwordInput = $("input[name='password']");
+    this.submitButton = $("button[type='submit']");
+    this.registerLink = $(By.linkText("Create new account"));
+  }
 
   @Step("Login with '{username}':'{password}'")
   @Nonnull
--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
@@ -1,25 +1,36 @@
 package guru.qa.niffler.test.web;
 
-import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.SelenideDriver;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.User;
-import guru.qa.niffler.jupiter.annotation.meta.WebTest;
+import guru.qa.niffler.jupiter.extension.NonStaticBrowserExtension;
 import guru.qa.niffler.jupiter.extension.UserExtension;
 import guru.qa.niffler.model.UserDataJson;
 import guru.qa.niffler.page.LoginPage;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
 
+import java.util.List;
+
+import static guru.qa.niffler.utils.SelenideUtils.chromeConfig;
+import static guru.qa.niffler.utils.SelenideUtils.firefoxConfig;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@WebTest
 class LoginTest {
 
   private static final Config CFG = Config.getInstance();
 
+  @RegisterExtension
+  private final NonStaticBrowserExtension browserExtension = new NonStaticBrowserExtension();
+  private final SelenideDriver chromeDriver = new SelenideDriver(chromeConfig);
+
   @User
   @Test
   void mainPageShouldBeDisplayedAfterSuccessLogin(UserDataJson user) {
-    Selenide.open(CFG.frontUrl(), LoginPage.class)
+    browserExtension.drivers().add(chromeDriver);
+
+    chromeDriver.open(CFG.frontUrl());
+    new LoginPage(chromeDriver)
         .doLogin(user.username(), user.testData().password())
         .checkThatPageLoaded();
   }
@@ -27,8 +38,13 @@ class LoginTest {
   @User
   @Test
   void userShouldStayOnLoginPageAfterLoginWithBadCredentials(UserDataJson user) {
-    Selenide.open(CFG.frontUrl(), LoginPage.class)
-        .doLogin(user.username(), UserExtension.DEFAULT_PASSWORD + "!");
-    assertEquals("Login to Niffler", Selenide.title());
+    SelenideDriver firefoxDriver = new SelenideDriver(firefoxConfig);
+    browserExtension.drivers().addAll(List.of(chromeDriver, firefoxDriver));
+
+    chromeDriver.open(CFG.frontUrl());
+    new LoginPage(chromeDriver).doLogin(user.username(), UserExtension.DEFAULT_PASSWORD + "!");
+    assertEquals("Login to Niffler", chromeDriver.title());
+    firefoxDriver.open(CFG.frontUrl());
+    new LoginPage(firefoxDriver).goRegisterPage();
   }
 }
