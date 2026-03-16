2fa4c95bf497fd4c474909a7a14c5fab5f8d8c65
alekulikov <99183267+alekulikov@users.noreply.github.com>
2026-03-16 20:03:21 +0300

    lesson_10.2_hw - complete (#17)
    Co-authored-by: alekulikov <aleksandr.a.kulikov@mvideo.ru>

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/test/web/LoginTest.java
@@ -3,12 +3,17 @@ package guru.qa.niffler.test.web;
 import com.codeborne.selenide.SelenideDriver;
 import guru.qa.niffler.config.Config;
 import guru.qa.niffler.jupiter.annotation.User;
-import guru.qa.niffler.jupiter.extension.NonStaticBrowserExtension;
+import guru.qa.niffler.jupiter.converter.BrowserArgumentConverter;
+import guru.qa.niffler.jupiter.extension.StaticBrowserExtension;
 import guru.qa.niffler.jupiter.extension.UserExtension;
 import guru.qa.niffler.model.UserDataJson;
 import guru.qa.niffler.page.LoginPage;
+import guru.qa.niffler.utils.Browser;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.provider.EnumSource;
 
 import java.util.List;
 
@@ -21,16 +26,18 @@ class LoginTest {
   private static final Config CFG = Config.getInstance();
 
   @RegisterExtension
-  private final NonStaticBrowserExtension browserExtension = new NonStaticBrowserExtension();
+  private static final StaticBrowserExtension browserExtension = new StaticBrowserExtension();
   private final SelenideDriver chromeDriver = new SelenideDriver(chromeConfig);
 
   @User
-  @Test
-  void mainPageShouldBeDisplayedAfterSuccessLogin(UserDataJson user) {
-    browserExtension.drivers().add(chromeDriver);
+  @ParameterizedTest
+  @EnumSource(Browser.class)
+  void mainPageShouldBeDisplayedAfterSuccessLogin(@ConvertWith(BrowserArgumentConverter.class)
+                                                  SelenideDriver driver, UserDataJson user) {
+    browserExtension.drivers().add(driver);
 
-    chromeDriver.open(CFG.frontUrl());
-    new LoginPage(chromeDriver)
+    driver.open(CFG.frontUrl());
+    new LoginPage(driver)
         .doLogin(user.username(), user.testData().password())
         .checkThatPageLoaded();
   }
